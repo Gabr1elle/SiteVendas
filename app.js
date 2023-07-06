55c938d8147f242fe0cce9e8a698b553eb95e095
Gabr1elle <132375538+Gabr1elle@users.noreply.github.com>
2023-07-06 16:58:53 -0300

    commit init

---
+++ app.js
@@ -0,0 +1,58 @@
+const vm = new Vue({
+	el: "#app",
+	data: {
+		produtos: [],
+		produto: false,
+		carrinho: [],
+		carrinhoTotal: 0,
+	},
+
+	filters: {
+		numeroPreco(valor) {
+			return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+		}
+	},
+
+	methods: {
+		fetchProdutos() {
+			fetch("./api/produtos.json")
+				.then(r => r.json())
+				.then(r => {
+					this.produtos = r;
+				})
+
+		},
+
+		fetchProduto(id) {
+			fetch(`./api/produtos/${id}/dados.json`)
+				.then(r => r.json())
+				.then(r => {
+					this.produto = r;
+				});
+		},
+
+		abrirModal(id){
+			this.fetchProduto(id);
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		},
+
+		fecharModal({ target, currentTarget }) {
+			if(target === currentTarget)
+			this.produto = false;
+		},
+
+		adicionarItem() {
+			this.produto.estoque--;
+			const {id, nome, preco} = this.produto;
+			this.carrinho.push({id, nome, preco});
+		},
+
+	},
+	created() {
+		this.fetchProdutos();
+	},
+})
+
